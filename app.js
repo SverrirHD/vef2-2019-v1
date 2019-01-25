fba29ec56bfd7b9f1acfedb6b2e8270aea865cfc
SverrirHD <shd4@hi.is>
2019-01-25 18:03:14 +0000

    skil

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-/* todo, setja upp express */
+/**
+ * Todo:
+ *  2. Fara í gegnum kröfur í verkefna lýsingu
+ *  3. Búa til 404 síðu
+ */
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const lectures = require('./lectures')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+
+app.use('/', lectures);
+
+function notFoundHandler(req, res, next) {
+  res.status(404).send('404 - Síða fannst ekki');
+}
+
+app.use(notFoundHandler);
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
